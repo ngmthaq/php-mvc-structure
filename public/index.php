52b75b21d73ba80ceaf051908a25fed8374174f7
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-11-16 18:46:32 +0700

    Init .env

--- public/index.php
+++ public/index.php
@@ -4,10 +4,13 @@ session_start();
 
 use Core\App;
 use Core\Request;
+use Dotenv\Dotenv;
 
 require_once("../vendor/autoload.php");
 require_once("../routes/index.php");
 
+$dotenv = Dotenv::createImmutable("../");
+$dotenv->load();
 $request = new Request();
 $app = new App($request, $router);
 $app->run();
