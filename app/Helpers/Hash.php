6d44f8f3de1006fec46b5ecca6bda4148739d902
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-11-19 12:25:14 +0700

    Handle helpers

--- app/Helpers/Hash.php
+++ app/Helpers/Hash.php
@@ -2,15 +2,21 @@
 
 namespace App\Helpers;
 
+use Exception;
+
 final class Hash
 {
-    public static function make()
+    final public static function make(string $string, string $algo = "sha256")
     {
-        //
+        if (in_array($algo, hash_algos())) {
+            return hash($algo, $string);
+        }
+
+        throw new Exception("Hashing algorithms is not supported");
     }
 
-    public static function check()
+    final public static function check(string $raw, string $hashed, string $algo = "sha256"): bool
     {
-        //
+        return hash_equals($hashed, self::make($raw, $algo));
     }
 }
--- app/Helpers/Str.php
+++ app/Helpers/Str.php
@@ -4,23 +4,45 @@ namespace App\Helpers;
 
 final class Str
 {
-    public static function lower()
+    final public static function lower(string $string): string
     {
-        //
+        return strtolower($string);
     }
 
-    public static function upper()
+    final public static function upper(string $string): string
     {
-        //
+        return strtoupper($string);
     }
 
-    public static function capital()
+    final public static function capital(string $string): string
     {
-        //
+        return ucwords($string);
     }
 
-    public static function slug()
+    final public static function slug(string $string): string
     {
-        //
+        $search = array(
+            '#(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)#',
+            '#(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)#',
+            '#(ì|í|ị|ỉ|ĩ)#',
+            '#(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)#',
+            '#(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)#',
+            '#(ỳ|ý|ỵ|ỷ|ỹ)#',
+            '#(đ)#',
+            '#(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)#',
+            '#(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)#',
+            '#(Ì|Í|Ị|Ỉ|Ĩ)#',
+            '#(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)#',
+            '#(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)#',
+            '#(Ỳ|Ý|Ỵ|Ỷ|Ỹ)#',
+            '#(Đ)#',
+            "/[^a-zA-Z0-9\-\_]/",
+        );
+        $replace = array('a', 'e', 'i', 'o', 'u', 'y', 'd', 'A', 'E', 'I', 'O', 'U', 'Y', 'D', '-');
+        $string = preg_replace($search, $replace, $string);
+        $string = preg_replace('/(-)+/', '-', $string);
+        $string = strtolower($string);
+
+        return $string;
     }
 }
